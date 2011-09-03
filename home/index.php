3d1cb300684116d3054e1012b6d30264caf0e856
Dusan Novakovic <ndusan@gmail.com>
2011-09-03 22:55:04 +0200

    small fix

--- home/index.php
+++ home/index.php
@@ -62,7 +62,7 @@
         <?php endif; ?>
     </div>
     <div class="homeSocial">
-        social plugin
+        <iframe src="http://www.facebook.com/plugins/facepile.php?app_id=118374028264322" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:400px; height: 300px;" allowTransparency="true"></iframe>
     </div>
 </div>
 

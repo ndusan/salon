894db8cbe39de99c018105b092429b6f77e4907a
emil <emil@ajduk.com>
2011-09-04 01:43:52 +0200

    bla

--- aboutus/index.php
+++ aboutus/index.php
@@ -1,5 +1,5 @@
 <ul class="products">
-    <li class="noTop noBot">
+    <li class="noTop">
         <h2>Ukratko o nama</h2>
         <p>Nakon dugogodišnjeg iskustva i mnogih zadovoljnih mušterija, studio lepote "Lan Team" i danas može da se pohvali visokokvalitetnom uslugom. Posetite nas i uverite se i sami po čemu se naš salon razlikuje od drugih, po čemu smo jedinstveni u našu profesiju.</p>
     </li>

acf76ae9c58c4153f428e7be24ac85b9bd71265f
emil <emil@ajduk.com>
2011-09-03 21:59:33 +0200

    stuff

--- home/index.php
+++ home/index.php
@@ -1,4 +1,43 @@
-HOME
+<div class="banner">
+    banner carousel
+</div>
+<div class="landing">
+    <h1>Welcome to Lan Team</h1>
+    <h2>BEAUTY SALON</h2>
+    <p>
+        Kako ćete prepoznati Alexandria proizvode ( tretman ) i Alexandria stručnjaka?
+    </p>
+    <p>
+        Alexandria proizvodi se uvoze direktno iz Kanade, nalaze se u plavo belim kutijama i flašicama, pune se fabrički i prošli su visoke sanitarne kontrole.
+        Sertifikovani Alexandria stručnjaci poseduju originalne diplome i Alexandria postere koji su nalaze na vidno postavljenim mestima.
+    </p>
+    <p>
+        Koža se obavezno prvo očisti Alexandria preseptom, nakon čega se stavlja tonik za proširenje pora i Alexandria specijalni puder.
+        Tretman se radi isključivo rukavicama. Šećerna pasta se zagreva jedino u grejačima, gde se pasta zagreje do temperature tela ( 37 stepeni ).
+        Nakon tretmana koža se smiruje Alexandria restorom i tonikom. 
+    </p>
+
+</div>
+<div class="home">
+    <div class="homeNews">
+        <? include_once 'public/news.php'; ?>
+        <?php if (!empty($_news)): ?>
+            <!-- News -->
+            <ul id="news" class="news">
+                <?php foreach ($_news as $n): ?>
+                    <li>
+                        <h3><?php echo $n['naslov']; ?></h3>
+                        <span><?php echo $n['datum']; ?></span>
+                        <p><?php echo $n['tekst']; ?></p>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+    </div>
+    <div class="homeSocial">
+        social plugin
+    </div>
+</div>
 
 
 
@@ -11,29 +50,6 @@ HOME
 <script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:send href="http://salonlepote.co.rs" font="arial"></fb:send>
 
 
-
-
-
-<!--Facepile of people who liked salonlepote.co.rs -->
-<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
-<fb:facepile href="http://salonlepote.co.rs" size="large" width="200" max_rows="10"></fb:facepile>
-
-
-
-
-<? include_once 'public/news.php';?>
-<?php if(!empty($_news)):?>
-<!-- News -->
-<ul id="news">
-  <?php foreach($_news as $n): ?>
-  <li>
-      <h3><?php echo $n['naslov']; ?></h3>
-      <span><?php echo $n['datum']; ?></span>
-      <p><?php echo $n['tekst']; ?></p>
-  </li>
-  <?php endforeach; ?>
-</ul>
-<?php endif;?>
 
 
 

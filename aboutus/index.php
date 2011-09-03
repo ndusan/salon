07a02d0da554c9ec4f07a2e8606437531c28b558
emil <emil@ajduk.com>
2011-09-04 01:42:00 +0200

    galerije

--- aboutus/index.php
+++ aboutus/index.php
@@ -5,6 +5,17 @@
             Nakon dugogodišnjeg iskustva i mnogih zadovoljnih mušterija, studio lepote "Lan Team" i danas može da se pohvali visokokvalitetnom uslugom. Posetite nas i uverite se i sami po čemu se naš salon razlikuje od drugih, po čemu smo jedinstveni u našu profesiju.
         </p>
     </li>
+    <li class="noBot">
+        
+        <ul class="gallery">
+            <li><a href="../public/images/mud.jpg" rel="lightbox"><img alt="MUD PUDDLE" width="200" height="200" src="../public/images/mud.jpg" /></a></li>
+            <li><a href="../public/images/mud.jpg" rel="lightbox"><img alt="MUD PUDDLE" width="200" height="200" src="../public/images/mud.jpg" /></a></li>
+            <li class="last"><a href="../public/images/mud.jpg" rel="lightbox"><img alt="MUD PUDDLE" width="200" height="200" src="../public/images/mud.jpg" /></a></li>
+            <li><a href="../public/images/mud.jpg" rel="lightbox"><img alt="MUD PUDDLE" width="200" height="200" src="../public/images/mud.jpg" /></a></li>
+            <li><a href="../public/images/mud.jpg" rel="lightbox"><img alt="MUD PUDDLE" width="200" height="200" src="../public/images/mud.jpg" /></a></li>
+            <li class="last"><a href="../public/images/mud.jpg" rel="lightbox"><img alt="MUD PUDDLE" width="200" height="200" src="../public/images/mud.jpg" /></a></li>
+        </ul>
+    </li>
 </ul>
 
 

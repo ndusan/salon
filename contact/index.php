65c0a9e694bd6d031f6db26aed5b0955d061611e
Dusan Novakovic <ndusan@gmail.com>
2011-09-11 23:41:15 +0200

    add gmap

--- contact/index.php
+++ contact/index.php
@@ -10,6 +10,6 @@
         <p>064 1515895</p>
         </div>
         <div class="homeSocial">
-            mapu odje
+            <iframe width="350" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?q=1+Jelisavete+Na%C4%8Di%C4%87,+Beograd,+%D0%A6%D0%B5%D0%BD%D1%82%D1%80%D0%B0%D0%BB%D0%BD%D0%B0+%D0%A1%D1%80%D0%B1%D0%B8%D1%98%D0%B0&amp;hl=en&amp;ie=UTF8&amp;sll=44.819438,20.469488&amp;sspn=0.006377,0.014398&amp;vpsrc=0&amp;t=m&amp;hq=&amp;hnear=Jelisavete+Na%C4%8Di%C4%87,+Stari+Grad,+Belgrade,+City+of+Belgrade,+Central+Serbia,+Serbia&amp;ll=44.819412,20.469503&amp;spn=0.021187,0.029955&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?q=1+Jelisavete+Na%C4%8Di%C4%87,+Beograd,+%D0%A6%D0%B5%D0%BD%D1%82%D1%80%D0%B0%D0%BB%D0%BD%D0%B0+%D0%A1%D1%80%D0%B1%D0%B8%D1%98%D0%B0&amp;hl=en&amp;ie=UTF8&amp;sll=44.819438,20.469488&amp;sspn=0.006377,0.014398&amp;vpsrc=0&amp;t=m&amp;hq=&amp;hnear=Jelisavete+Na%C4%8Di%C4%87,+Stari+Grad,+Belgrade,+City+of+Belgrade,+Central+Serbia,+Serbia&amp;ll=44.819412,20.469503&amp;spn=0.021187,0.029955&amp;z=14&amp;iwloc=A&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small>
         </div>
     </div>

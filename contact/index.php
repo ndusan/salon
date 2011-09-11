8d08aa561cf9f0007a5aa6e70b1237a92587fd72
emil <emil@ajduk.com>
2011-09-11 23:17:42 +0200

    contact

--- contact/index.php
+++ contact/index.php
@@ -1,18 +1,15 @@
-<ul class="products">
-    <li class="noTop noBot">
-        <h2>Salon lepote Lan Team</h2>
-        <p>Kontakt osoba:</p>
-        <p>Zorka Jonić</p>
+<div class="home">
+        <div class="homeNews">
+                <h2>Salon lepote Lan Team</h2>
+        <p>Kontakt osoba: Zorka Jonić</p>
         <h2>Adresa:</h2>
-        <p>ul.Jelisavete Načić (bivša Pavla Papa) broj 1</p>
+        <p>ul. Jelisavete Načić (bivša Pavla Papa) broj 1</p>
         <h2>Telefon:</h2
-        <p>011-3370025</p>
+        <p>011 3370025</p>
         <h2>Mob.tel:</h2>
-        <p>064-1515895</p>
-    </li>
-</ul>
-
-
-<div>
-    gogole mapa
-</div>
+        <p>064 1515895</p>
+        </div>
+        <div class="homeSocial">
+            mapu odje
+        </div>
+    </div>

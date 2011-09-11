7986f939f39a0cc2ff226ec1922e8ed710d9c5c7
emil <emil@ajduk.com>
2011-09-11 02:49:12 +0200

    mrshava

--- home/header.php
+++ home/header.php
@@ -1,9 +1,21 @@
 <div class="banner" id="slides">
     <div class="slides_container">
-        <div style="background:#fce43e url(../public/images/slide1.jpg) scroll no-repeat 0 0; "></div>
-        <div style="background:#feee39 url(../public/images/slide2.jpg) scroll no-repeat 0 0; "></div>
-        <div style="background:#fc5db5 url(../public/images/slide3.jpg) scroll no-repeat 0 0; "></div>
-        <div style="background:#f4f45b url(../public/images/slide4.jpg) scroll no-repeat 0 0; "></div>
+        <div style="background:#fce43e url(../public/images/slide1.jpg) scroll no-repeat 0 0; ">
+            <h1>Epilacija, nešto sasvim novo kod nas</h1>
+            <img src="../public/images/epilacija.png" />
+        </div>
+        <div style="background:#feee39 url(../public/images/slide2.jpg) scroll no-repeat 0 0; ">
+            <h1>Estetski i hirurshki pedikir</h1>
+            <img src="../public/images/pedikir.png" />
+        </div>
+        <div style="background:#fc5db5 url(../public/images/slide3.jpg) scroll no-repeat 0 0; ">
+            <h1>Manikir i izlivanje noktiju</h1>
+            <img src="../public/images/manikir.png" />
+        </div>
+        <div style="background:#f4f45b url(../public/images/slide4.jpg) scroll no-repeat 0 0; ">
+            <h1>Tretmani lica</h1>
+            <img src="../public/images/lice.png" />
+        </div>
     </div>
 </div>
 <script>
